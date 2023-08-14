14a70ed167fca9876f92102f0d75ea14113cd46c
domm <dommferger@gmail.com>
2023-08-14 01:56:09 -0400

    This took too long...

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// index.js
+const mainElement = document.getElementById("main");
+  mainElement.remove();
+  const newHeader = document.createElement("h1");
+  newHeader.id = "victory";
+  newHeader.innerHTML = "DOMINICK is the champion";
+  document.body.append(newHeader);
+
+const element = document.createElement("div");
+
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+
